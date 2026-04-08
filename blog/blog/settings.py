d332ba5f6e7a8f575a09d9d52ac10dc9f4942ff9
Maksim Savoskin <telikonazir@gmail.com>
2026-04-08 03:58:02 -0400

    добавлена модель пользователя

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -44,6 +44,15 @@ DATABASES = {
     }
 }
 
+INSTALLED_APPS = [
+    #'django.contrib.admin',
+    #'django.contrib.auth',
+    #'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'login',
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
---
+++ blog/login/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class User(models.Model):
+    login = models.CharField(max_length=200, unique=True)
+    password = models.CharField(max_length=200)
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    age = models.PositiveIntegerField()
+    balance = models.FloatField()
